49cd68dfc26f42f7d8c0e8c063ada968e7ecf830
Tomislav Kristo <Tomkristo@gmail.com>
2017-10-05 19:23:33 -0400

    Completed slingshot script

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+    public GameObject launchPoint;
+
+    private void Awake() {
+        Transform launchpointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchpointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseEnter () {
+        // print("Slingshot: OnMouseEnter()");
+        launchPoint.SetActive(true);
+	}
+	
+	void OnMouseExit () {
+        // print("Slingshot:OnMouseeExit()");
+        launchPoint.SetActive(false);
+	}
+}
