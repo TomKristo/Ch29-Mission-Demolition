c9e02a22d80bcd66df0fe44c15b41a818e9a4c2f
Tomislav Kristo <Tomkristo@gmail.com>
2017-10-06 11:05:07 -0400

    Camera reset script

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -9,17 +9,33 @@ public class FollowCam : MonoBehaviour {
     public GameObject poi;
     public float camZ;
 
-    private void Awake()
+    void Awake()
     {
         S = this;
         camZ = this.transform.position.z;
     }
 
-    private void Update()
+    void FixedUpdate()
     {
-        if (poi == null) return;
+        Vector3 destination;
 
-        Vector3 destination = poi.transform.position;
+        if (poi == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            destination = poi.transform.position;
+            if(poi.tag == "Projectile")
+            {
+                if (poi.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    poi = null;
+                    return;
+                }
+            }
+        }
+
         destination.x = Mathf.Max( minXY.x, destination.x);
         destination.y = Mathf.Max( minXY.y, destination.y); 
         destination = Vector3.Lerp(transform.position, destination, easing);
